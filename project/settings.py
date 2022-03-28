322aba84171e9e6c19f7bdb6044332e0e3741ab4
mksifat101 <mksifat101@gmail.com>
2022-03-28 23:45:21 +0600

    Deploy On Heroku Server

--- project/settings.py
+++ project/settings.py
@@ -139,4 +139,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
