db368d1b6d0d2858c969dda4a6825161ee6e8171
mksifat101 <mksifat101@gmail.com>
2022-03-29 02:11:51 +0600

    fix main slider link issus

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,10 @@ from product.models import Product
 
 
 def home(request):
+    slid = Product.objects.all()
     prod = Product.objects.all()
     data = {
         'prod': prod,
+        'slid': slid,
     }
     return render(request, 'pages/home.html', data)
