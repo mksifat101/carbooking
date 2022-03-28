f6b7e57957ceb543be0820d7ab4376302eea3d8f
mksifat101 <mksifat101@gmail.com>
2022-03-28 20:17:12 +0600

    Deploy On Heroku Server

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 # ALLOWED_HOSTS = []
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'carprebookingapp.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
